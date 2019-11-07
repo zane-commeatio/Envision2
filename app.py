54c3f201b502ba6e77ed260bcaeae7678f3c0b89
Zanea <anthony.93460@gmail.com>
2019-11-07 19:22:19 +0100

    debug

--- app.py
+++ app.py
@@ -6,10 +6,11 @@ app = Flask(__name__)
 def index():
 
     if request.method == 'POST':
-        f = request.files['uploaded_picture']
-        
-        f.save(secure_filename(f.filename))
-        return render_template('index.html', debug = str(f.filename))
+        #f = request.files['uploaded_picture']
+        #f.save(secure_filename(f.filename))
+        print('DEBUG')
+        files = request.files
+        return render_template('index.html', debug = str(files))
         
 
     return render_template('index.html', debug = None)
